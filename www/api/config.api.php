bcdbdfd61756f167fdcfafe6080e2ba983d87f75
Nexis <mrtwidget@gmail.com>
2017-04-15 20:52:52 -0400

    Added API Filtering
    Added filtering options to API and updated the request processing
    function to be less clunky.

--- www/api/config.api.php
+++ www/api/config.api.php
@@ -6,7 +6,7 @@ $config["database"]["password"] = "password";
 $config["database"]["database"] = "unturned";
 
 // database tables
-$config["database"]["tables"]["livemap_servers"] = "livemap_servers";
+$config["database"]["tables"]["livemap_server"] = "livemap_server";
 $config["database"]["tables"]["livemap_data"] = "livemap_data";
 $config["database"]["tables"]["livemap_chat"] = "livemap_chat";
 ?>
--- www/api/livemap.api.php
+++ www/api/livemap.api.php
@@ -9,10 +9,13 @@
  * @version 0.2.0
  */
 class API {
+    /* MySQL Connection */
     public $mysql;
+
+    /* Configuration Settings */
     public $config;
 
-    function __construct($server_id = null) {
+    function __construct($server_id = null, $filter = null) {
         // require config
         require(__DIR__ . "/config.api.php");
         $this->config = $config;
@@ -23,7 +26,7 @@ class API {
 
         // process and return request
         if ($server_id != null) {
-            $request = self::return_server_status($server_id);
+            $request = self::process_request($server_id, $filter);
             echo json_encode($request);
         }
     }
@@ -50,41 +53,34 @@ class API {
     }
 
     /**
-     * Return Server Status
+     * Process Request
      *
      * This function queries the database by filtering results based on the `server_id` identifier passed by `$_GET[livemap]`,
-     * and returns a combined array of all associated livemap tables [`livemap_servers`,`livemap_data`,`livemap_chat`]
+     * and returns a combined array of all associated livemap tables [`livemap_server`,`livemap_data`,`livemap_chat`]
      * @param string $id        ServerID associated with table column `server_id`
-     * @return array $result    Returned array of table result queries combining `livemap_servers`, `livemap_data`, and `livemap_chat`
+     * @return array $result    Returned array of table result queries combining `livemap_server`, `livemap_data`, and `livemap_chat`
      */
-    public function return_server_status($server_id) {
-        $livemap_servers = $this->config["database"]["tables"]["livemap_servers"];
+    public function process_request($server_id, $filter = null) {
+        // tables
+        $livemap_server = $this->config["database"]["tables"]["livemap_server"];
         $livemap_data = $this->config["database"]["tables"]["livemap_data"];
         $livemap_chat = $this->config["database"]["tables"]["livemap_chat"];
 
-        // sterilize input and query server status
-        $query_server_status = $this->mysql->prepare("SELECT * FROM $livemap_servers WHERE server_id = :server_id");
-        $query_server_status->bindValue(':server_id', $server_id, PDO::PARAM_STR);
-        $query_server_status->execute();
-        $server_status = $query_server_status->fetch(PDO::FETCH_ASSOC);
+        // queries
+        $livemap_server_query = "SELECT * FROM $livemap_server WHERE server_id = :server_id";
+        $livemap_data_query = "SELECT * FROM $livemap_data WHERE last_refresh <> last_disconnect AND server_id = :server_id";
+        $livemap_chat_query = "SELECT * FROM $livemap_chat WHERE timestamp > DATE_SUB(NOW(), INTERVAL 5 MINUTE) AND server_id = :server_id";
 
-        // if server status is not empty, generate a result
-        if ($server_status != false) {
-            // query online players
-            $query_player_data = $this->mysql->query("SELECT * FROM $livemap_data WHERE last_refresh <> last_disconnect");
-            $player_data = $query_player_data->fetchAll(PDO::FETCH_ASSOC);
-
-            // query last 5 minutes of world chat
-            $query_world_chat = $this->mysql->query("SELECT * FROM $livemap_chat WHERE timestamp > DATE_SUB(NOW(), INTERVAL 5 MINUTE)");
-            $world_chat = $query_world_chat->fetchAll(PDO::FETCH_ASSOC);
-
-            // combine the arrays
-            $result["status"] = $server_status;
-            $result["players"] = $player_data;
-            $result["world_chat"] = $world_chat;
+        // loop through each table
+        foreach ($this->config["database"]["tables"] as $table) {
+            // filter results if $filter is set
+            if ($filter == null || $filter == $table) {
+                $query = $this->mysql->prepare(${$table . "_query"});
+                $query->bindValue(':server_id', $server_id, PDO::PARAM_STR);
+                $query->execute();
+                $result[$table] = $query->fetchAll(PDO::FETCH_ASSOC);
+            }
         }
-
-        // return result
         return $result;
     }
 }
@@ -95,7 +91,7 @@ class API {
  * it will return a http response code of 400 (Bad Request)
  */
 if (isset($_GET["livemap"])) {
-    $api = new API($_GET["livemap"]);
+    $api = new API($_GET["livemap"], (isset($_GET["filter"]) ? $_GET["filter"] : null));
 } else {
     http_response_code(400); // bad request
 }
